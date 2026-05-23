a71243c08dcc0ead3cc617fc46a7017dd0e15e7c
analystAdendi-arch <emmanueladendi@gmail.com>
2026-05-23 18:13:36 +0300

    Add Footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 HouseHub. All rights reserved.</p>
+      <p>Making apartment hunting easy and secure.</p>
+    </footer>
+  );
+};
+
+export default Footer;
